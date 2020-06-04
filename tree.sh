23376dc1167fd1d2b3e9917bfb6959eda087196c
Maximiliano González-Serrano <pacomax1994@gmail.com>
2020-06-04 14:35:19 -0500

    Update tree.sh

--- tree.sh
+++ tree.sh
@@ -1,3 +1,10 @@
+#This is a particular script for Salamanders project
+
+
+export LC_ALL=en_US.utf-8 #two lines for ASQII phyton issues 
+export LANG=en_US.utf-8
+source activate qiime2-2019.10 #activate the envieroment where is qiime2
+
 qiime alignment mafft --i-sequences rep_Illumina_V4.qza --p-n-threads 12 --o-alignment aligned-rep_Illumina_V4.qza
 qiime alignment mask --i-alignment aligned-rep_Illumina_V4.qza --o-masked-alignment masked-aligned-rep_Illumina_V4.qza
 qiime phylogeny fasttree --i-alignment masked-aligned-rep_Illumina_V4.qza --p-n-threads 12 --o-tree unrooted-tree-v4.qza
