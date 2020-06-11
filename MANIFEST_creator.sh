e5e0486a9de7c5910832171a86a53edb0df2a963
Francisco Maximiliano González-Serrano <pacomax1994@gmail.com>
2020-06-10 23:55:47 -0500

    Update MANIFEST_creator.sh

--- MANIFEST_creator.sh
+++ MANIFEST_creator.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #This script create a MANIFEST file with the sequences from the directory Illumina_V4 created by clean_and_merge.sh
 dir=$(pwd)/Illumina_V4/
-list_seq=$(ls Illumina_V4| grep "_good.fastq" | sed 's/_good.fastq$//g')
+list_seq=$(ls Illumina_V4| grep "_good.fastq$" | sed 's/_good.fastq$//g')
 dates=$(date +"%d_%m_%Y")
 touch Illumina_V4/MANIFEST_${dates}.txt
 echo "sample-id absolute-filepath" >> Illumina_V4/MANIFEST_${dates}.txt
