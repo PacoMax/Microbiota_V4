84ea654d1b6436653a5caa0e5f51307022a6bbb5
Francisco Maximiliano González-Serrano <pacomax1994@gmail.com>
2020-07-09 20:30:22 -0500

    Update tree.sh

--- tree.sh
+++ tree.sh
@@ -1,11 +1,29 @@
-#This is a particular script for Salamanders project
+if [ "$1" == "-h" ]; then
+echo ""
+echo "  Welcome to tree.sh"
+echo "  This script makes an aligment and a tree of representative otus"
+echo "  To run this program it's necesary to activate qiime2 enviroment"
+echo ""
+echo ""
+echo "  Usage: `basename $0` [dir] [cpus]"
+echo ""
+echo "  dir:"
+               echo "          Name of the directory where the files are:"
+echo ""
+echo "  cpus:"
+               echo  "         interger: number of cpus"
+echo ""
+echo ""
 
+exit 0
+fi
+dir=${1?Error: No directory specified. Please, ask for help (./tree.sh -h)}
+cpus=${2?Error: No number of cpus specified. Please, ask for help (./tree.sh -h)}
 
-export LC_ALL=en_US.utf-8 #two lines for ASQII phyton issues 
+export LC_ALL=en_US.utf-8 #two lines for ASQII phyton issues
 export LANG=en_US.utf-8
-source activate qiime2-2019.10 #activate the envieroment where is qiime2
 
-qiime alignment mafft --i-sequences rep_Illumina_V4.qza --p-n-threads 12 --o-alignment aligned-rep_Illumina_V4.qza
-qiime alignment mask --i-alignment aligned-rep_Illumina_V4.qza --o-masked-alignment masked-aligned-rep_Illumina_V4.qza
-qiime phylogeny fasttree --i-alignment masked-aligned-rep_Illumina_V4.qza --p-n-threads 12 --o-tree unrooted-tree-v4.qza
-qiime phylogeny midpoint-root --i-tree unrooted-tree-v4.qza --o-rooted-tree rooted-tree-v4.qza
+qiime alignment mafft --i-sequences ${dir}/rep_* --p-n-threads 12 --o-alignment ${dir}/aligned-rep_${dir}.qza
+qiime alignment mask --i-alignment ${dir}/aligned-rep_${dir}.qza --o-masked-alignment masked-aligned-rep_${dir}.qza
+qiime phylogeny fasttree --i-alignment masked-aligned-rep_${dir}.qza --p-n-threads 12 --o-tree unrooted-tree-${dir}.qza
+qiime phylogeny midpoint-root --i-tree unrooted-tree-${dir}.qza --o-rooted-tree rooted-tree-${dir}.qza
