115627a5929e32e4954f9575d8e393766f7efef4
Francisco Maximiliano González-Serrano <pacomax1994@gmail.com>
2020-09-01 19:57:32 -0500

    Update download_sra.sh

--- download_sra.sh
+++ download_sra.sh
@@ -5,8 +5,7 @@ mkdir ~/ncbi/public/sra #directory where the SRA files wil be download.
 for i in $(cat SRA.list) #SRA.list is a file containing the SRA IDs
 do
 prefetch -v $i
-fastq-dump --outdir reads --skip-technical -I -W --split-files ~/ncbi/
-public/sra/$i.sra
+fastq-dump --outdir reads --skip-technical -I -W --split-files ~/ncbi/public/sra/$i.sra
 rm ~/ncbi/public/sra/$i.sra
 sleep 2 #wait 2 sec because of the data traffic
 done
