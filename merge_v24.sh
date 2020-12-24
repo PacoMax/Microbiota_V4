d1e03035bb851005193fab36c4d1297690fe7edb
Francisco Maximiliano González-Serrano <pacomax1994@gmail.com>
2020-12-24 09:08:37 -0600

    Update merge_v24.sh

--- merge_v24.sh
+++ merge_v24.sh
@@ -4,7 +4,7 @@ export LC_ALL=en_US.utf-8 #two lines for ASQII phyton issues
 export LANG=en_US.utf-8
 
 
-qiime feature-table merge-seqs --i-data Illumina_V2_single/rep_Illumina_V2_single.qza --i-data Illumina_V4/rep_Illumina_V4_09_07_2020.qza --o-merged-data insertion-rep-seqs_${dates}.qza
-qiime feature-table merge --i-tables Illumina_V2_single/table_Illumina_V2_single.qza --i-tables Illumina_V4/table_Illumina_V4_03_09_2020.qza --o-merged-table insertion-V24-table_${dates}.qza
+qiime feature-table merge-seqs --i-data Illumina_V2_single/rep_Illumina_V2_single.qza --i-data Illumina_V4/rep_Illumina_V4_09_07_2020.qza --i-data Illumina_V34/rep_Illumina_V34*.qza --o-merged-data insertion-rep-seqs_${dates}.qza
+qiime feature-table merge --i-tables Illumina_V2_single/table_Illumina_V2_single.qza --i-tables Illumina_V4/table_Illumina_V4*.qza --i-tables Illumina_V34/table_Illumina_V34*.qza --o-merged-table insertion-V234-table_${dates}.qza
 #download silva tree
 qiime fragment-insertion sepp --i-representative-sequences insertion-rep-seqs_${dates}.qza --p-threads 12 --o-tree insertion-tree_${dates}.qza --i-reference-database sepp-refs-silva-128.qza --o-placements insertion-placements_${dates}.qza
