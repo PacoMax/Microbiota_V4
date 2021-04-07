50200c43c8d08758f1ad20dff37acbec77864ff0
Francisco Maximiliano González-Serrano <pacomax1994@gmail.com>
2021-04-06 23:52:37 -0500

    Update MANIFEST_creator.sh

--- MANIFEST_creator.sh
+++ MANIFEST_creator.sh
@@ -5,4 +5,4 @@ list_seq=$(ls Illumina_V4| grep "_good.fastq$" | sed 's/_good.fastq$//g')
 dates=$(date +"%d_%m_%Y")
 touch Illumina_V4/MANIFEST_${dates}.txt
 echo "sample-id absolute-filepath" >> Illumina_V4/MANIFEST_${dates}.txt
-for i in $list_seq; do echo "$i ${dir}${i}_good.fastq" >> Illumina_V4/MANIFEST_${dates}.txt; done
+for i in $list_seq; do echo "$i ${dir}/${i}_good.fastq" >> Illumina_V4/MANIFEST_${dates}.txt; done
