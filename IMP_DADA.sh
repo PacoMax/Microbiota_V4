e7f06055d9a9f34f26f82c37edb830c75b6f86b4
Francisco Maximiliano González-Serrano <pacomax1994@gmail.com>
2022-07-06 18:52:20 -0500

    Update IMP_DADA.sh

--- IMP_DADA.sh
+++ IMP_DADA.sh
@@ -1,5 +1,6 @@
 for i in $(cat PATHs_Illumina);
 do
+cd $i;
 j=$(echo "$i" | rev | cut -d '_' -f1 | rev | sed 's/V//g');
-import_and_dada2.sh MANIFEST* 20 10000 23000 $j
+import_and_dada2.sh MANIFEST* 20 10000 23000 $j;
 done
