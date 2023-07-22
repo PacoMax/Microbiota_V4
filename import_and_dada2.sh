2ccff3f976ab7c302ea1c697fab6eda5fcac1472
Francisco Maximiliano González-Serrano <pacomax1994@gmail.com>
2023-07-22 12:38:05 -0600

    Update import_and_dada2.sh

--- import_and_dada2.sh
+++ import_and_dada2.sh
@@ -8,7 +8,7 @@ echo ""
 echo "    This pipeline cut all the reads to 100 bp length"
 echo "    To change that, you can edit this script with a text editor in the option --p-trunc-len (default 100bp)"
 echo ""
-echo "  Usage: `basename $0` [MANIFEST] [cpus] [learn] [rare] [region]"
+echo "  Usage: `basename $0` [MANIFEST] [cpus] [learn] [region]"
 echo ""
 echo "  MANIFEST:"
                echo "         txt file which should look like:"
@@ -56,7 +56,7 @@ qiime tools import --type 'SampleData[SequencesWithQuality]' --input-path $MANIF
 if [ $region == 4 ] || [ $region == 34 ]
         then
         qiime cutadapt trim-single --i-demultiplexed-sequences ${folder}_${dates}.qza --p-front GTGTGYCAGCMGCCGCGGTAA --p-error-rate 0.2 --p-cores $cpus --o-trimmed-sequences trimmed-seqs_${folder}_${dates}.qza --verbose --p-no-discard-untrimmed
-        qiime dada2 denoise-single --i-demultiplexed-seqs trimmed-seqs_${folder}_${dates}.qza --p-trunc-len 100 --p-n-threads $cpus --o-table table_${folder}_${dates}.qza --p-n-reads-learn $learn --p-max-ee $rare --verbose --o-representative-sequences rep_${folder}_${dates}.qza --o-denoising-stats stats_${folder}_${dates}.qza
+        qiime dada2 denoise-single --i-demultiplexed-seqs trimmed-seqs_${folder}_${dates}.qza --p-trunc-len 100 --p-n-threads $cpus --o-table table_${folder}_${dates}.qza --p-n-reads-learn $learn --verbose --o-representative-sequences rep_${folder}_${dates}.qza --o-denoising-stats stats_${folder}_${dates}.qza
 else
-        qiime dada2 denoise-single --i-demultiplexed-seqs ${folder}_${dates}.qza --p-trunc-len 100 --p-n-threads $cpus --o-table table_${folder}_${dates}.qza --p-n-reads-learn $learn --p-max-ee $rare --verbose --o-representative-sequences rep_${folder}_${dates}.qza --o-denoising-stats stats_${folder}_${dates}.qza
+        qiime dada2 denoise-single --i-demultiplexed-seqs ${folder}_${dates}.qza --p-trunc-len 100 --p-n-threads $cpus --o-table table_${folder}_${dates}.qza --p-n-reads-learn $learn --verbose --o-representative-sequences rep_${folder}_${dates}.qza --o-denoising-stats stats_${folder}_${dates}.qza
 fi
