f992957785443c3c5ac2032984cb30dfd0a29c9a
Francisco Maximiliano González-Serrano <pacomax1994@gmail.com>
2023-07-26 20:05:52 -0600

    Update LMM_alpha.r

--- LMM_alpha.r
+++ LMM_alpha.r
@@ -3,78 +3,95 @@
 
 #Reading data
 
-infossm<-read.csv("Super_table_ss_10_06_21.csv")
+infossm<-read.csv("Super_table_26_07_2023.csv")
 #centralizing the data
 infossm[,c(15:37)]<-apply(infossm[,c(15:37)],2,function(x){scale(x, center = TRUE, scale = TRUE)})
-#Log transfromations for evading right skew
-infossm$faith_pd<-log(infossm$faith_pd)
 
-#Step one
+#infossm_c<-infossm[infossm$origin!="captive",]
+
+##Step one
+#Correlation between environmental and alpha diversity metrics
+
+correla<-rcorr(as.matrix(infossm_c[,c(15:37,42:45)]),type = "pearson")
+x<-correla[["r"]]
+write.csv(file="Stats_r_v2.csv", x)
+x<-correla[["P"]]
+write.csv(file="Stats_p_v2.csv", x)
+correlas<-rcorr(as.matrix(infossm_c[,c(15:37,42:45)]),type = "spearman")
+x<-correlas[["r"]]
+write.csv(file="Stats_s_26_07_2023.csv", x)
+x<-correlas[["P"]]
+write.csv(file="Stats_sp_26_07_2023.csv", x)
+
+#Some variables are deleted (rho>0.7)
+#tm_min,bio3,bio5,bio7,bio11,bio13,bio14,bio15,bio16
+
+##Step two
+
+#Deleting variables with Spearman rho >= 0.7
+No<-c("tm_min","bio3", "bio5", "bio7", "bio11","bio13","bio14","bio15","bio16")
+No_id<-which(colnames(infossm_c)%in%No)
+id<-c(15:37,45)
+Si_id<-id[-which(id%in%No_id)]
+infossmw<-infossm_c[,Si_id]
+
 #Stepwise regression
+library("sjstats")
 
-
-infosslo<-infossm[,c(15:37,42)]
-
-
-FitStart<-lm(faith_pd ~ 1, data=na.omit(infosslo))
-
-FitAll<-lm(faith_pd ~ ., data=na.omit(infosslo))
-
-
+FitStart<-lm(shannon_entropy ~ 1, data=na.omit(infossmw))
+FitAll<-lm(shannon_entropy ~ ., data=na.omit(infossmw))
 step(FitStart,direction = "both",scope = formula(FitAll))
 
+#Model_1
+lm1<-lm(formula = shannon_entropy ~ bio19 + pre + tm_max + elevation + 
+     bio8 + bio12 + bio17 + bio1 + bio10 + bio18 + bio6 + bio2, 
+   data = na.omit(infossmw))
 
+##Step three
 
+#Adding as fixed factors Family and Habitat (morphotype) and Study as random effects
 
-
-lmo2.1<-lm(formula = faith_pd ~ bio18 + tm_max + bio3 + bio1 + tm_min + 
-             elevation + bio17 + bio10 + bio12 + bio14 + bio13 + bio19 + 
-             bio9 + bio5 + pre, data = na.omit(infosslo))
-
-
-#Step two
-
-#Deleting variables with pearson coefficient >= .7
-
-
-lmo2.1_a<-lm(formula = faith_pd ~ tm_max + pre + bio1 + bio3 + bio5 +
-             bio9 + bio12 + bio17 + bio18 + bio19, 
-           data = na.omit(infosslo))
-
-
-
-
-#Step three
-
-#Adding as fixed factors Family and Habitat and Study as random effects
-
-infossm$habitat<-as.numeric(as.factor(infossm$morphotype))-1
-
-lmm2.1_c<-lmer(formula = faith_pd ~ tm_max + pre + bio1 + bio3 + bio5 +
-              bio9 + bio12 + bio17 + bio18 + bio19 +
-              habitat + (1|Study) + Family, REML=F, data = na.omit(infossm))
-
+lmm2.1_c<-lmer(formula = shannon_entropy ~ bio19 + pre + tm_max + elevation + 
+                 bio8 + bio12 + bio17 + bio1 + bio10 + bio18 + bio6 + bio2 +
+                 morphotype + (1|Study) + Family, REML=F, data = na.omit(infossm_c))
 
 #Cheking collinearity and deleting variables with VIF > 11 
 
 library("performance")
-check_collinearity(lmm2.1_c)
 
 
-lmm2.1_s<-lmer(formula = faith_pd ~ tm_max + pre + bio1 + bio3 + bio5 +
-               bio9 + bio17 + bio18 + bio19 + 
-               habitat + (1|Study) + Family, REML=F, data = na.omit(infossm))
+tabla_VIF<-check_collinearity(lmm2.1_c)
 
 
 
-#Contrasting models
+write.csv(tabla_VIF, "table_VIF_lmm_26_07_2023.csv")
+
+
+#Deleting variables bio1, bio10, bio6
+
+lmm2.1_s<-lmer(formula = shannon_entropy ~ bio19 + pre + tm_max + elevation + 
+                 bio8 + bio12 + bio17 + bio18 + bio2 +
+                 morphotype + (1|Study) + Family, REML=F, data = na.omit(infossm_c))
+
+
+
+#Cheking collinearity again
+tabla_VIF2<-check_collinearity(lmm2.1_s)
+
+
+write.csv(tabla_VIF2, "table_VIF_lmm2_26_07_2023.csv")
+
+
+#Comparing the two models
 
 anova(lmm2.1_s, lmm2.1_c)
-
 
 library("MuMIn")
 
 r.squaredGLMM(lmm2.1_c)
 r.squaredGLMM(lmm2.1_s)
 
-save(infossm,lmm2.1_s, file = "model_data.RData")
+#The best model based on AIC was lmm2.1_s
+save(infossm,lmm2.1_s, file = "model_data_26_07_2023.RData")
+
+plot_model(lmm2.1_s, vline.color = "red")
